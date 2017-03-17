21340c944a7e47591b205fea5ff8dcf57491add7
Marcus Vinicius <marcus@vulpdigital.com.br>
2017-03-16 23:58:09 -0300

    Esboço do About, parallax e Galeria

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function navBarEffect() {
+    var nav = document.getElementById("topNav");
+    if (nav.className === "nav-links") {
+        nav.className += " responsive";
+    } else {
+        nav.className = "nav-links";
+    }
+}
