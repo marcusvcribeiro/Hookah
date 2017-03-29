7cb33780cc2a17804f4816331250d06e15999b7d
Marcus Vinicius <marcus@vulpdigital.com.br>
2017-03-29 00:16:37 -0300

    Scroll menu e alterações de chamadas HTML

--- js/main.js
+++ js/main.js
@@ -6,3 +6,29 @@ function navBarEffect() {
         nav.className = "nav-links";
     }
 }
+
+$(document).ready(function() {
+    $('#map').addClass('desliga-frame'); // set the mouse events to none when doc is ready
+
+    $('#overlay').on("mouseup", function() { // lock it when mouse up
+        $('#map').addClass('desliga-frame');
+        //somehow the mouseup event doesn't get call...
+    });
+    $('#overlay').on("mousedown", function() { // when mouse down, set the mouse events free
+        $('#map').removeClass('desliga-frame');
+    });
+
+    $("#map").mouseleave(function() { // because the mouse up doesn't work... 
+        $('#map').addClass('desliga-frame'); // set the pointer events to none when mouse leaves the map area
+        // or you can do it on some other event
+    });
+});
+
+//Scroll Menu
+var $doc = $('html, body');
+$(".nav li a").click(function() {
+    $doc.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 700);
+    return false;
+});
