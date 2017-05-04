07f6620d15c7937626a128ce2faff1dca864f23d
Icaro Figueiredo <icaroofigueiredo@gmail.com>
2017-05-03 23:58:23 -0300

    Form no esquema, enviando para o meu email

--- js/main.js
+++ js/main.js
@@ -89,3 +89,98 @@ function mascaraData(inputData, e){
     return false;
 }
 
+/* Máscaras ER */
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)
+}
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+function mtel(v){
+    v=v.replace(/\D/g,"");             //Remove tudo o que não é dígito
+    v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+    v=v.replace(/(\d)(\d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
+    return v;
+}
+function id( el ){
+	return document.getElementById( el );
+}
+window.onload = function(){
+	id('telefone').onkeyup = function(){
+		mascara( this, mtel );
+	}
+}
+
+/* MASCARA HORARIO */
+var mask = function (val) {
+    val = val.split(":");
+    return (parseInt(val[0]) > 19)? "HZ:M0" : "H0:M0";
+}
+
+pattern = {
+    onKeyPress: function(val, e, field, options) {
+        field.mask(mask.apply({}, arguments), options);
+    },
+    translation: {
+        'H': { pattern: /[0-2]/, optional: false },
+        'Z': { pattern: /[0-3]/, optional: false },
+        'M': { pattern: /[0-5]/, optional: false }
+    },
+    placeholder: 'Horário - HH:MM'
+};
+
+$(document).ready(function () {
+  $("#horario").mask(mask, pattern);
+});
+
+/*VALIDAR EMAIL */
+function validacaoEmail(field) {
+    usuario = field.value.substring(0, field.value.indexOf("@"));
+    dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+
+    if ((usuario.length >=1) &&
+        (dominio.length >=3) && 
+        (usuario.search("@")==-1) && 
+        (dominio.search("@")==-1) &&
+        (usuario.search(" ")==-1) && 
+        (dominio.search(" ")==-1) &&
+        (dominio.search(".")!=-1) &&      
+        (dominio.indexOf(".") >=1)&& 
+        (dominio.lastIndexOf(".") < dominio.length - 1)) {
+    document.getElementById("msgemail").innerHTML="E-mail válido";
+    alert("E-mail valido");
+    }
+    else{
+    document.getElementById("msgemail").innerHTML="<font color='red'>E-mail inválido </font>";
+    alert("E-mail invalido");
+    }
+}
+
+/*MODAL FORMULARIO */
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
