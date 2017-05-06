dab594cef19514fb44ed9d34768697141274264b
Icaro Figueiredo <icaroofigueiredo@gmail.com>
2017-05-06 18:29:40 -0300

    tem treta no js, olha no console depois de preenc
    preecher o form.

--- js/main.js
+++ js/main.js
@@ -162,17 +162,37 @@ function validacaoEmail(field) {
 // Get the modal
 var modal = document.getElementById('myModal');
 
+// ERRO TA AQUI!!! NÃO CARREGA ISSO QUANDO TUDO É DIGITADO, O RESULTADO É TODOS ESSES CAMPOS COMO SE ESTIVESSE
+// VAZIOS
+var nome = document.getElementById("nome").value;
+var email = document.getElementById("email").value;
+var telefone = document.getElementById("telefone").value;
+var data = document.getElementById("data").value;
+var horario = document.getElementById("horario").value;
+var qtd = document.getElementById("qtd").value;
+
+
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
 
+
+// When the user clicks the button, open the modal 
+
+    // if(
+    //     (nome.length >= 1) &&
+    //     (email.length >= 10) &&
+    //     (telefone.length >= 11) &&
+    //     (data.length >= 5) &&
+    //     (horario.length >= 5) &&
+    //     (qtd.length >= 1)
+    // ) {
+    btn.onclick = function() {
+        modal.style.display = "block";   
+    }
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
